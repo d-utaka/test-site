0de6e97c92bafe773d14c7f18dacceb05da5abf9
d-utaka <33531046+d-utaka@users.noreply.github.com>
2026-04-15 18:54:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const message = document.querySelector("#message")
-message.textContent = "alo alo"
+const WOFF_ID = "Gb06_5WrOvf5v40zzrEZnQ"
+
+woff
+    .init({
+        woffId: WOFF_ID
+    })
+    .then(() => {
+        const message = document.querySelector("#message")
+        message.textContent = "in woff.init().then()"
+    })
+    .catch((err) => {
+        console.log(err.code, err.message);
+    })
